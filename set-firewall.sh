02d9c80c8e2459e8d57c305e0af31c9da4387b66
ANTI VIRAL <109937537+ANTI-VIRAL@users.noreply.github.com>
2025-05-15 19:29:26 +0000

    Create set-firewall.sh

---
+++ set-firewall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# PORT VPS RELAY kamu
+RELAY_PORT="12263"
+
+# FLUSH tapi jaga koneksi SSH biar gak lost
+sudo iptables -F
+sudo iptables -X
+
+# Default policy
+sudo iptables -P OUTPUT DROP
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD DROP
+
+# Allow koneksi SSH (jaga jangan keputus ya sayang)
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
+
+# Allow koneksi keluar ke port relay (destination ALL)
+sudo iptables -A OUTPUT -p tcp --dport $RELAY_PORT -j ACCEPT
+
+# Allow DNS (kalau pakai domain)
+sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+
+# Allow localhost
+sudo iptables -A OUTPUT -o lo -j ACCEPT
+
+# (Optional) Simpan rules
+sudo apt-get install iptables-persistent -y
+sudo netfilter-persistent save
